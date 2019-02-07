e9e186af8b67df8ef2b62d60df4ba3545f15f4cf
Dave Soler <dave@agorasource.com>
2019-02-07 14:43:47 -0500

    Initial angular project creation

---
+++ portfolio/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatButtonModule} from '@angular/material';
+
+
+import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {RouterModule} from '@angular/router';
+import {appRoutes} from './app.routing';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
