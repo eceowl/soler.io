ae077902237a9f4326363ef930022486d9a7fc85
Dave Soler <dave@agorasource.com>
2019-05-19 12:04:00 -0400

    Add favicon

--- portfolio/src/app/constants/projects.ts
+++ portfolio/src/app/constants/projects.ts
@@ -2,5 +2,5 @@ import {Project} from "../models/project";
 
 export const projects = [
   new Project("Website Redesign", "Wyndsor Farm", 'A mobile friendly redesign of Wyndsor\s 10 year old website that is up to par with the quality of the beautiful farm itself.', "https://www.wyndsorfarm.com", "assets/img/WyndsorFarmThumbnail.PNG","far fa-window-maximize"),
-  new Project('Instagram Makeover', 'Wyndsor Farm', 'Took over control of Wyndsor Farm Instagram account and increased followers and engagement dramatically', 'https://www.instagram.com/wyndsor_farm/', "assets/img/WyndsorFarmInstagramThumbnail.PNG","fab fa-instagram"),
+  new Project('Instagram Rebranding', 'Wyndsor Farm', 'Took over control of Wyndsor Farm Instagram account and increased followers and engagement dramatically', 'https://www.instagram.com/wyndsor_farm/', "assets/img/WyndsorFarmInstagramThumbnail.PNG","fab fa-instagram"),
 ]
