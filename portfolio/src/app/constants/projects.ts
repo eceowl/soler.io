d4d974464b38771158a3ba034dc56da12877a781
Dave Soler <dave@agorasource.com>
2019-05-21 17:12:00 -0400

    Minor text updates

--- portfolio/src/app/constants/projects.ts
+++ portfolio/src/app/constants/projects.ts
@@ -1,6 +1,16 @@
 import {Project} from "../models/project";
 
 export const projects = [
-  new Project("Website Redesign", "Wyndsor Farm", 'A mobile friendly redesign of Wyndsor\s 10 year old website that is up to par with the quality of the beautiful farm itself.', "https://www.wyndsorfarm.com", "assets/img/WyndsorFarmThumbnail.PNG","far fa-window-maximize"),
-  new Project('Instagram Rebranding', 'Wyndsor Farm', 'Took over control of Wyndsor Farm Instagram account and increased followers and engagement dramatically', 'https://www.instagram.com/wyndsor_farm/', "assets/img/WyndsorFarmInstagramThumbnail.PNG","fab fa-instagram"),
-]
+  new Project("Website Redesign",
+    "Wyndsor Farm",
+    'A mobile friendly redesign of Wyndsor\s 10 year old website that is up to par with the quality of the beautiful farm itself.',
+    "https://www.wyndsorfarm.com",
+    "assets/img/WyndsorFarmThumbnail.PNG",
+    "far fa-window-maximize"),
+  new Project('Instagram Rebranding',
+    'Wyndsor Farm',
+    'Operating Wyndsor Farm Instagram account in order to increase followers and customer engagement.',
+    'https://www.instagram.com/wyndsor_farm/',
+    "assets/img/WyndsorFarmInstagramThumbnail.PNG",
+    "fab fa-instagram"),
+];
--- portfolio/src/app/constants/staff.ts
+++ portfolio/src/app/constants/staff.ts
@@ -5,18 +5,15 @@ export const staff = [
   new StaffMember("Dave Soler",
     "Lead Software Developer",
     "My name is Dave Soler and I've been creating software in the Philadelphia Area for over 6 years. " +
-    "I have created responsive marketing websites for small businesses, mobile (Android and iOS) applications for Labor Unions, " +
-    "and even am soon-to-be published for my software contributions to a study at the Children's Hospital of Philadelphia. " +
-    "Needless to say, I am qualified and capable of solving any business problem that comes my way",
-    ["Responsive Website Development", "Mobile App Development", "Ui/Ux Design"],
+    "I've created Android and iOS apps for teacher's and carpenter's unions, I've developed marketing websites for a local farm, " +
+    "I've even written software for the Children's Hospital of Philadelphia to help analyze patient data. " +
+    "Needless to say, I've seen it all and am confident I can help create software to solve your business needs.",
+    ["Responsive Website Development", "Mobile App Development", "IT Services", "Ui/Ux Design"],
     "assets/img/DaveHeadshot.png"),
 
   new StaffMember("Gill Soler",
     "Digital Content Strategist",
-    "My name is Gill Soler and Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
-    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley " +
-    "of type and scrambled it to make a type specimen book. " +
-    "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    "My name is Gill Soler and Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     ["Digital Content Strategy", "Copywriting", "Social Media Marketing", "Editorial Content"],
     "assets/img/GillHeadshot.jpg"),
 ];
